08bb6f1de280dc84352c4ec2e0d13cee96804185
Chris Jin <chris_0408@qq.com>
2018-07-10 10:02:12 +0800

    Update replace-docker-compose.sh

--- replace-docker-compose.sh
+++ replace-docker-compose.sh
@@ -1,9 +1,34 @@
 #! /bin/bash
-version=$1
-if [[ -z $1 ]]; then
-	echo "请输入版本号, 如: 1.0"
-	exit 1
-fi
-#替换docker-compose文件中第4行中有关镜像版本的关键字
-sed -i "4s/scmjd:.*$/scmjd:$version/" docker-compose.yml
-docker-compose up -d
+function changeserver {
+ read -p "please input server verison:" version
+ serverversion="$version"
+ sed -i "4s/scmjd:.*$/scmjd:$serverversion-test/" docker-compose.yml
+}
+function rubyapi {
+ read -p "please input rubyapi verison:" version
+ apiversion="$version"
+ sed -i "17s/jdruby:.*$/jdruby:$apiversion-tjd/" docker-compose.yml
+}
+function start {
+ docker-compose up -d
+}
+echo "----------------------------------"
+echo "please enter your choise:"
+echo "(1) Update server"
+echo "(2) Update rubyapi"
+echo "(3) Update web"
+echo "----------------------------------"
+read input
+case $input in
+    1)
+    changeserver
+    start ;;
+    2)
+    rubyapi
+    start ;;
+    3)
+    echo ToDo
+    start ;;
+    *)
+   echo "Sorry.wrong selection" ;;
+esac
